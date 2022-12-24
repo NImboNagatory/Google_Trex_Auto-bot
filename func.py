30a5985fa80dd64e993f1afc669d357aed0b0818
Nika Shinjikashvili <96537789+NImboNagatory@users.noreply.github.com>
2022-12-24 21:32:49 +0400

    v1.5

--- func.py
+++ func.py
@@ -7,29 +7,41 @@ class Trex_wrecker:
     def __init__(self):
         self.Day_Night = "Day"
         self.trex_location = None
-        self.jump_dist = 120
+        self.jump_dist = 160
         open("https://elgoog.im/t-rex/v2/", new=1)
 
     def find_trex(self):
         trex_location = locateOnScreen('data/Trex.PNG')
         if trex_location is not None:
             self.trex_location = trex_location
-            click(self.trex_location[0] + 100, self.trex_location[1] + 22)
+            click(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22)
             press('up')
 
     def check_obstacle(self):
-        pixel_cactus = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22)[0]
-        pixel_cactus_smol = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 31)[0]
-        pixel_bird = pixel(self.trex_location[0]+120, self.trex_location[1]-2)[0]
-        if pixel_cactus < 247:
-            press('up')
-            self.jump_dist += 2
-        elif pixel_cactus_smol < 247:
-            press('up')
-            self.jump_dist += 2
-        elif pixel_bird < 247:
-            start = time()
-            while time() - start < 5:
-                press('down')
+        # pixel_cactus = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22)[0]
+        # pixel_cactus_smol = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 31)[0]
+        # pixel_bird = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1])[0]
 
+        for Pixel in range(9):
+            pixel_c = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_bird = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1])[0]
+            if pixel_c < 247:
+                press('up')
+                self.jump_dist += 2
+            elif pixel_bird < 247:
+                start = time()
+                while time() - start < 100:
+                    press('down')
+                    self.jump_dist += 2
 
+        # if pixel_cactus < 247:
+        #     self.jump_dist += 2
+        #     press('up')
+        # if pixel_cactus_smol < 247:
+        #     self.jump_dist += 2
+        #     press('up')
+        # if pixel_bird < 247:
+        #     start = time()
+        #     while time() - start < 100:
+        #         self.jump_dist += 2
+        #         press('down')
