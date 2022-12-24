3e8b6b529395a0801c9651aabaf6851f80ace9d2
Nika Shinjikashvili <96537789+NImboNagatory@users.noreply.github.com>
2022-12-24 19:43:30 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from func import Trex_wrecker
+
+cheat = Trex_wrecker()
+while cheat.trex_location is None:
+    cheat.find_trex()
+
+
+def infinity():
+    while True:
+        yield
+
+
+for _ in infinity():
+    cheat.check_obst()
