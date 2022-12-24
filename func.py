4ec9aca5bd62fb4d3d24579ba6f89ec4ee051a88
Nika Shinjikashvili <96537789+NImboNagatory@users.noreply.github.com>
2022-12-24 21:43:25 +0400

    .

--- func.py
+++ func.py
@@ -23,11 +23,15 @@ class Trex_wrecker:
         # pixel_bird = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1])[0]
 
         for Pixel in range(9):
-            pixel_a = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_b = pixel(self.trex_location[0] + self.jump_dist + 1, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_c = pixel(self.trex_location[0] + self.jump_dist + 2, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_a = pixel(self.trex_location[0] + self.jump_dist-3, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_b = pixel(self.trex_location[0] + self.jump_dist-2, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_c = pixel(self.trex_location[0] + self.jump_dist-1, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_d = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_e = pixel(self.trex_location[0] + self.jump_dist + 1, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_f = pixel(self.trex_location[0] + self.jump_dist + 2, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_i = pixel(self.trex_location[0] + self.jump_dist + 3, self.trex_location[1] + 22 + Pixel)[0]
             pixel_bird = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1])[0]
-            if pixel_a < 247 or pixel_b < 247 or pixel_c < 247:
+            if pixel_a < 247 or pixel_b < 247 or pixel_c < 247 or pixel_d < 247 or pixel_e < 247 or pixel_f < 247 or pixel_i < 247:
                 press('up')
                 self.jump_dist += 2
             elif pixel_bird < 247:
