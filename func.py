1ca271d4e0aa0a25ed15f9b4c6e1389d54d0960c
NImboNagatory <lukanogga@gmail.com>
2022-12-26 19:03:40 +0400

    .

--- func.py
+++ func.py
@@ -18,39 +18,21 @@ class Trex_Wrecker:
             press('up')
 
     def check_obstacle(self):
-        # pixel_cactus = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22)[0]
-        # pixel_cactus_smol = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 31)[0]
-        # pixel_bird = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1])[0]
 
         for Pixel in range(9):
-            pixel_a = pixel(self.trex_location[0] + self.jump_dist-3, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_b = pixel(self.trex_location[0] + self.jump_dist-2, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_c = pixel(self.trex_location[0] + self.jump_dist-1, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_d = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_e = pixel(self.trex_location[0] + self.jump_dist + 1, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_f = pixel(self.trex_location[0] + self.jump_dist + 2, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_i = pixel(self.trex_location[0] + self.jump_dist + 3, self.trex_location[1] + 22 + Pixel)[0]
-            pixel_bird_a = pixel(self.trex_location[0] + self.jump_dist - 1, self.trex_location[1])[0]
-            pixel_bird = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1])[0]
-            pixel_bird_b = pixel(self.trex_location[0] + self.jump_dist + 1, self.trex_location[1])[0]
-            if pixel_a < 247 or pixel_b < 247 or pixel_c < 247 or pixel_d < 247 or pixel_e < 247 or pixel_f < 247 or pixel_i < 247:
+            pixel_a_ = pixel(self.trex_location[0] + self.jump_dist-8, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_b_ = pixel(self.trex_location[0] + self.jump_dist-4, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_c_ = pixel(self.trex_location[0] + self.jump_dist-2, self.trex_location[1] + 22 + Pixel)[0]
+            pixel_d_ = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 31)[0]
+            pixel_e_ = pixel(self.trex_location[0] + self.jump_dist + 2, self.trex_location[1] + 31 - Pixel)[0]
+            pixel_f_ = pixel(self.trex_location[0] + self.jump_dist + 4, self.trex_location[1] + 31 - Pixel)[0]
+            pixel_i_ = pixel(self.trex_location[0] + self.jump_dist + 8, self.trex_location[1] + 31 - Pixel)[0]
+            if pixel_i_ < 247 or pixel_f_ < 247 or pixel_e_ < 247 or pixel_d_ < 247 or pixel_c_ < 247 or pixel_b_ < 247 or pixel_a_ < 247:
+                self.jump_dist += 2
                 press('up')
-                self.jump_dist += 2
-            elif pixel_bird_a < 247 or pixel_bird < 247 or pixel_bird_b < 247:
-                start = time()
-                self.jump_dist += 2
-                while time() - start < 100:
-                    press('down')
+            # elif pixel_bird_a < 247 or pixel_bird < 247 or pixel_bird_b < 247:
+            #     start = time()
+            #     self.jump_dist += 2
+            #     while time() - start < 100:
+            #         press('down')
 
-
-        # if pixel_cactus < 247:
-        #     self.jump_dist += 2
-        #     press('up')
-        # if pixel_cactus_smol < 247:
-        #     self.jump_dist += 2
-        #     press('up')
-        # if pixel_bird < 247:
-        #     start = time()
-        #     while time() - start < 100:
-        #         self.jump_dist += 2
-        #         press('down')
