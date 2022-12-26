5502c9dbee29775db2569d6b4011103eaf6a4932
NImboNagatory <lukanogga@gmail.com>
2022-12-26 20:47:20 +0400

    .

--- func.py
+++ func.py
@@ -12,20 +12,24 @@ class TrexWrecker:
 
     def find_trex(self):
         trex_location = locateOnScreen('data/Trex.PNG')
+        # finds trex location on screen and saves its coordinates
         if trex_location is not None:
             self.trex_location = trex_location
             click(self.trex_location[0] + self.jump_dist, self.trex_location[1])
             press('up')
 
     def check_obstacle(self):
-        while True:
+        # infinite loop
+        for _ in iter(int, 1):
             pixel_c_ = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] + 28)[0]
             pixel_b_ = pixel(self.trex_location[0] + self.jump_dist, self.trex_location[1] - 2)[0]
+            # check if cactus is ahead
             if pixel_c_ < 247:
                 press('up')
                 sleep(0.2)
                 keyDown('down')
                 keyUp('down')
+                # check if bird is up ahead
             elif pixel_b_ < 247:
                 keyDown('down')
                 sleep(0.5)
